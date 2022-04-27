d2465c7221824b958619c2d083a35bcced473b8e
tbwahacker <twaloadam@gmail.com>
2022-04-27 21:31:15 +0300

    first commit

---
+++ app/src/main/java/tz/co/digital_survey_swahili_app/MainActivity.java
@@ -0,0 +1,16 @@
+package tz.co.digital_survey_swahili_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Digital_Survey_Swahili_App We gonna use drone cameras to survey,drawing maps and measure distances of area of plots using android app
+    }
+}
